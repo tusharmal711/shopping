d053db110b1b7ed390a5a4f7d0c13bb269f979ca
tusharmal711 <tusharmal711@gmail.com>
2025-07-06 22:50:51 +0530

    d

--- backand/server.js
+++ backand/server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://shopping-beta-three.vercel.app/",
+    origin: "https://shopping-beta-three.vercel.app",
     credentials: true,
   })
 );
